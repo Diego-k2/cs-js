f959e74f887877c17fd6773ab7511db74c068a17
Diego-k2 <degosantosiva@gmail.com>
2022-10-27 15:04:01 -0300

    conversoes

--- Modulo 2 (Variaveis)/booleanos.js
+++ Modulo 2 (Variaveis)/booleanos.js
@@ -6,3 +6,6 @@ const isPaid = false;
 console.log("" == false);
 
 console.log(undefined == false);
+
+
+console.log(typeof(isPaid));
---
+++ Modulo 2 (Variaveis)/conversao.js
@@ -0,0 +1,15 @@
+//tipo de dado
+//booleanos 
+
+
+const numero = 456;
+const numeroString = Number("456");
+
+
+// console.log(numero === numeroString); //compara valor e tipo
+
+// console.log(numero == numeroString); //compara somente o valor 
+
+
+
+console.log(numeroString === numero); //forma de converter
