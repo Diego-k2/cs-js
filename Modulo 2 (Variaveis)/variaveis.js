ddc47c7ca65f7206b87237b57f982cc46b7f9b12
Diego-k2 <degosantosiva@gmail.com>
2022-10-27 14:23:46 -0300

    variaveis

---
+++ Modulo 2 (Variaveis)/variaveis.js
@@ -0,0 +1,43 @@
+//var 
+// var altura = 5;
+// var comprimento = 7;
+// area = altura * comprimento;
+// var area;
+// console.log(area + ", usando var");
+
+// //let
+// let forma = "retangulo";
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+
+// if(forma === "retangulo"){
+//     area = altura * comprimento;
+// } else if(forma === "triangulo") {
+//     area = (altura * comprimento)/2;
+// } else {
+//     area = "Não é retangulo nem triangulo";
+// }
+
+// console.log(area);
+
+const forma = "quadrado";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === "quadrado"){
+    area = altura * comprimento;
+} else if(forma === "triangulo") {
+    area = (altura * comprimento)/2;
+} else {
+    area = "Não é retangulo nem triangulo";
+}
+console.log(area);
+
+
+
+
+
+
