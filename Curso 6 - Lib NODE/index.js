90742957b469ac2e089e359002fd11c2b592907b
Diego-k2 <dSilva@netsoft.com>
2023-01-18 16:10:24 -0300

    Modulo 2

--- Curso 6 - Lib NODE/index.js
+++ Curso 6 - Lib NODE/index.js
@@ -1,6 +1,47 @@
+import fs from "fs";
 import chalk from "chalk";
 
-console.log(chalk.blue('olá mundo'));
-console.log('olá mundo');
+//Função que dropa um erro toda vez que é chamada 
+function trataErro(err){
+    throw new Error(chalk.red(err.code, "Não há arquivo no diretorio"));
+}
+
+// function pegaArquivo(caminhoArquivo){
+//     const encode = "utf-8"
+//     fs.readFile(caminhoArquivo, encode, (err, result) =>{
+//         if(err){
+//             trataErro(err);
+//         } 
+//         console.log(chalk.green(result));
+//     } )
+// }
+
+//Criando via função assincrona usando com THEN
+// function pegaArquivo(caminhoArquivo){
+//     const encode = "utf-8";
+
+//     fs.promises.readFile(caminhoArquivo, encode)
+//         .then((texto) => console.log(chalk.green(texto))) //Pegando a respotas caso de certo
+//         .catch((erro) => trataErro(erro)) //Pegando a resposta caso de erro 
+
+// }
+
+//Criando via função assincrona usando com async/await
+//Deixa a gente escrever de forma muito semelhante a um codigo sincrono 
+async function pegaArquivo(caminhoArquivo){
+    
+    try{
+        const enconde = "utf-8";
+        const texto = await fs.promises.readFile(caminhoArquivo, enconde); 
+        console.log(chalk.green(texto));
+    } catch(err) { //precisamos fazer assim para pegar o erro 
+        trataErro(err)
+    }
+    
+
+}
 
 
+
+// pegaArquivo("./arquivos/");
+pegaArquivo("./arquivos/texto.md");
